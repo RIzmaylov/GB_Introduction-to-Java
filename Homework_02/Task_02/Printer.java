bfdbc8a930b369f69594e258e911d7cf2dd09829
RIzmaylov <izya.man18@gmail.com>
2024-01-24 16:32:57 +0300

    Add homework_02 2 task

--- Homework_02/Task_02/Printer.java
+++ Homework_02/Task_02/Printer.java
@@ -10,20 +10,45 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 
 class BubbleSort {
+    static String logPath = "Homework_02/Task_02/log.txt";
     private static File log;
     private static FileWriter fileWriter;
-
-    public static void logger() {
-      SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm");
-      Date date = new Date();
-      
-      System.out.println(formatter.format(date));
-
-    }
-    
+    static SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm");
+    static Date date = new Date();
 
     public static void sort(int[] mas) {
-      logger();
+      try {
+        fileWriter = new FileWriter(logPath);
+      } catch (IOException e) {
+        e.printStackTrace();
+      }
+      for (int i = 0; i < mas.length; ++i) {
+        String tempMas = "[";
+        int cnt = 0;
+        for (int j = 0; j < mas.length - 1; ++j) {
+          if (mas[j] > mas[j + 1]) {
+            int temp = mas[j];
+            mas[j] = mas[j + 1];
+            mas[j + 1] = temp;
+            cnt++;
+            // swap(&mas[j], &mas[j + 1]);
+          }
+          tempMas += mas[j];
+          if (j != mas.length - 2) tempMas += ", ";
+        }
+        try {
+          fileWriter.write(formatter.format(date) + " " + tempMas + ", " + mas[mas.length - 1] + "]\n");
+        } catch (IOException e) {
+          e.printStackTrace();
+        }
+        
+        if (cnt == 0) break;
+      }
+      try {
+        fileWriter.close();
+      } catch (IOException e) {
+        e.printStackTrace();
+      }
   }
 }
 // Не удаляйте этот класс - он нужен для вывода результатов на экран и проверки
@@ -33,7 +58,7 @@ public class Printer{
       
       if (args.length == 0) {
         // При отправке кода на Выполнение, вы можете варьировать эти параметры
-        arr = new int[]{9, 4, 8, 3, 1};
+        arr = new int[]{10, 9, 8, 7, 6, 5, 4, 3, 2, 1};
       }
       else{
         arr = Arrays.stream(args[0].split(", "))
@@ -44,7 +69,7 @@ public class Printer{
       BubbleSort ans = new BubbleSort();      
       ans.sort(arr);
 
-      try (BufferedReader br = new BufferedReader(new FileReader("log.txt"))) {
+      try (BufferedReader br = new BufferedReader(new FileReader("Homework_02/Task_02/log.txt"))) {
             String line;
             while ((line = br.readLine()) != null) {
                 System.out.println(line);
