9fe99c6ab7875adf7fdaf67f0d9ed54f13cc819c
RIzmaylov <izya.man18@gmail.com>
2024-02-01 17:14:17 +0300

    add homework_04

---
+++ Homework_04/Task_01/Printer.java
@@ -0,0 +1,44 @@
+package Homework_04.Task_01;
+
+/*
+ * Дан LinkedList с несколькими элементами разного типа. 
+ * В методе revert класса LLTasks реализуйте разворот этого списка без использования встроенного функционала.
+ */
+
+import java.util.LinkedList;
+
+class LLTasks {
+    public static LinkedList<Object> revert(LinkedList<Object> ll) {
+        // Напишите свое решение ниже
+        LinkedList<Object> result = new LinkedList<>();
+        for (Object object : ll) {
+            result.addFirst(object);
+        }
+        return result;
+    }
+}
+
+// Не удаляйте этот класс - он нужен для вывода результатов на экран и проверки
+public class Printer{ 
+    public static void main(String[] args) { 
+        LinkedList<Object> ll = new LinkedList<>();
+
+        if (args.length == 0 || args.length != 4) {
+        // При отправке кода на Выполнение, вы можете варьировать эти параметры
+            ll.add(1);
+            ll.add("One");
+            ll.add(2);
+            ll.add("Two");
+        } else {
+            ll.add(Integer.parseInt(args[0]));
+            ll.add(args[1]);
+            ll.add(Integer.parseInt(args[2]));
+            ll.add(args[3]);
+        }
+
+        LLTasks answer = new LLTasks();
+        System.out.println(ll);
+        LinkedList<Object> reversedList = answer.revert(ll);
+        System.out.println(reversedList);
+    }
+}
